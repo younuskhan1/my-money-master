86f4c635dabf7bc0f5f7ed783b03882ce0fbc1db
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-06 11:40:27 +0600

    saving calculation is about completed

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function getInputMoney(inputFieldId) {
     const inputField = document.getElementById(inputFieldId);
     const inputMoneyString = inputField.value;
     const inputMoney = parseFloat(inputMoneyString);
-    inputField.value = "";
+    // inputField.value = "";
     return inputMoney;
 }
+
 
 document.getElementById("calculate-button").addEventListener("click", function () {
     const monthlyTotalIncome = getInputMoney("input-income");
@@ -23,6 +24,33 @@ document.getElementById("calculate-button").addEventListener("click", function (
             const monthlyRemainingBalance = monthlyTotalIncome - totalMonthlyExpenses;
             document.getElementById("total-expense-amount").innerText = totalMonthlyExpenses;
             document.getElementById("total-balance-amount").innerText = monthlyRemainingBalance;
+        }
+
+    }
+
+})
+
+function getBalanceAmount(balanceFieldId) {
+    const balanceField = document.getElementById(balanceFieldId);
+    const balanceString = balanceField.innerText;
+    const balanceMoney = parseFloat(balanceString);
+    return balanceMoney;
+}
+document.getElementById("saving-button").addEventListener("click", function () {
+    const monthlyTotalIncome = getInputMoney("input-income");
+    const remainingBalanceAfterExpenses = getBalanceAmount("total-balance-amount");
+    const savingPercentage = getInputMoney("input-save");
+    if (savingPercentage < 0 || isNaN(savingPercentage)) {
+        alert("Please provide a positive numeric values");
+    } else {
+        const savingAmount = (monthlyTotalIncome * savingPercentage) / 100;
+
+        const ultimateRemainingBalance = remainingBalanceAfterExpenses - savingAmount;
+        if (ultimateRemainingBalance < 0) {
+            alert("Saving amount cannot be more than remaining balance after expenses.")
+        } else {
+            document.getElementById("Saving Amount").innerText = savingAmount;
+            document.getElementById("Remaining Balance").innerText = ultimateRemainingBalance;
         }
 
     }
